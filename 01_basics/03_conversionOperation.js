f3a21f0891b69ca8cfa77e8128c11a7f9460e3d7
prince pal <princepal12329@gmail.com>
2024-12-20 14:10:46 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score ="99abc"
+console.log(typeof score);
+console.log(typeof(score));
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1; false =>0
+
+ let isloggedIn = 1
+ let ans=Boolean(isloggedIn);
+ console.log(ans);
+
+ //.  ****************** opertion ********************
+
+ let value =3;
+ let negvalue=-value;
+ console.log(negvalue);
+    let str1="hello"
+    let str2=" prince"
+    let str3=str1+str2;
+    console.log(str3);
+    
+ 
+ 
