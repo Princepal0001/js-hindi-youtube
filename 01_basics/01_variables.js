3141812ed94e81e69133fe7acff4698009c865b2
prince pal <princepal12329@gmail.com>
2024-12-20 10:18:33 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 991156
+let accountEmail="prince@gmail.com"
+var accountPassword="prince@123"
+accountCity="Delhi"
+// accountId=2  // it can't be changed beacuse it is a constant value
+accountEmail="princepal@gmail.com"
+accountPassword="prince123123@"
+accountCity="uttarPradesh"
+let accountState;
+ console.log(accountId);
+ /*
+
+        let and var both are use for variables 
+            but
+        prefer not ti use var 
+        because of issue in block scope and funtional scope
+
+ */
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+ 
+ 
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we use using node js ,not browser
+console.log("prince pal")
+let name = "prince pal"  //  string type
+let age=18 // number 
+let isloggedIn=false // boolean
+console.log(typeof name);
+console.log(typeof undefined); // output is undefined
+console.log(typeof null);   // output is object
+
