12d3d755791f9ce5ce8203131700a5bb5800c308
prince pal <princepal12329@gmail.com>
2024-12-20 17:11:13 +0000

    primitive_nonPrimitive

---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,28 @@
+//Primitive
+// 7 types : string ,Number,Boolean,Null,undefined,symbol,bigInt
+ let name="prince"
+const score=100;
+const scoreValue=100.3
+const  isloggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=symbol('123')
+const anotherId=symbol('123')
+ 
+console.log(id===anotherId);
+const bignumber =  7867235746781n
+
+
+// (Non primitive) Reference
+// array,object,functions
+const heros =["ironman","batman","superman"];
+let myobj ={
+    name: "prince",
+    age : 22,
+}
+const myFunction = function(){
+    console.log("hello world")
+}
+
+
+
