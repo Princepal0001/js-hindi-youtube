65858370bf1cd0f8206512805366d66539d60923
prince pal <princepal12329@gmail.com>
2024-12-21 09:28:17 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,4 +5,29 @@ console.log(`my name is ${name} and my number is ${number}`);
 
 //  method 2
 const UserName= new String("prince");
-console.log(UserName);
+console.log(UserName); // prince 
+
+console.log(UserName[0]); // p
+
+console.log(UserName.length); // 6
+
+console.log(UserName.toUpperCase());  //PRINCE
+
+console.log(UserName.charAt(5));  // e
+
+console.log(UserName.indexOf('c'));  // 4
+
+const newsubstring = UserName.substring(0,3) // substring(include,exclude)
+console.log(newsubstring); // pri
+
+const newStringOne = "  prince pal  "
+console.log(newStringOne);  // (  prince pal  )
+
+console.log(newStringOne.trim());  // (prince pal)
+
+const url = "https://prince.com/prince%20pal";
+console.log(url.replace('%20','-'));    // https://prince.com/prince-20pal
+
+
+
+
