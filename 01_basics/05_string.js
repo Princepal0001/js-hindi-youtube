64cce3da7f102ef25df7d3eb156350fe6a40ea69
prince pal <princepal12329@gmail.com>
2024-12-21 13:45:01 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "prince"
+const number = 9911
+console.log(name +number +"value");
+console.log(`my name is ${name} and my number is ${number}`);
+
+//  method 2
+const UserName= new String("prince");
+console.log(UserName);
